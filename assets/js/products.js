c0d7bf9f75251ebe4b1f0c7776261ed371275150
m0trix-1 <hoangtrunglc25@gmail.com>
2026-07-17 01:08:11 +0700

    Add Starbucks product catalog to products.js

---
+++ assets/js/products.js
@@ -0,0 +1,28 @@
+const STARBUCKS_CATALOG = {
+    menuGrid: [
+        {
+            id: "pink-strawberry-frapp",
+            displayTag: "STRAWBERRY CREAM",
+            image: "assets/img/pink-strawberry.png",
+            alt: "Strawberry Cream Frappuccino"
+        },
+        {
+            id: "iced-strawberry-tea",
+            displayTag: "STRAWBERRY HIBISCUS",
+            image: "assets/img/strawberry-hibiscus.png",
+            alt: "Iced Strawberry Hibiscus"
+        },
+        {
+            id: "holiday-berry-mocha",
+            displayTag: "BERRY MOCHA",
+            image: "assets/img/red-holiday-cup.png",
+            alt: "Holiday Berry Mocha"
+        },
+        {
+            id: "mango-yellow-frapp",
+            displayTag: "MANGO PASSION",
+            image: "assets/img/mango-yellow.png",
+            alt: "Mango Passionfruit Frappuccino"
+        }
+    ]
+};
